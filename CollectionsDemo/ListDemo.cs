0a0e7216436aca5834a0c512df23f3abcc2bae34
sipra <sipramishra085@gmail.com>
2023-07-07 09:54:30 +0530

    [Sipra]Add:command of list

---
+++ CollectionsDemo/ListDemo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsDemo
+{
+    //List<T>class represents the list of objects which can be accessed with index.
+    //List is dynamic ,we can resize with items
+    public class ListDemo
+    {
+        //creating a list of integers
+        List<int> list= new List<int>();
+        public void Adding()
+        {
+            list.Add(10);
+            list.Add(11);
+            list.Add(13);
+            foreach(var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Count of list -"+list.Count);
+            Console.WriteLine("Capacity of list -" + list.Capacity);
+            list.Add(14);
+            list.Add(15);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Capacity of list -" + list.Capacity);
+            Console.WriteLine("Find elements -" + list.Contains(1));
+            Console.WriteLine("Remove elemrnt -" + list.Remove(14));
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("*********");
+            Console.WriteLine("removing element for a particular position");
+            list.RemoveAt(3);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("*********");
+            Console.WriteLine("Reverse element");
+            list.Reverse();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("*********");
+            Console.WriteLine("Sorting the list element");
+            list.Add(2);
+            list.Sort();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("*********");
+            Console.WriteLine("Delete element");
+            list.Clear();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ CollectionsDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsDemo
+{
+    public  class Program
+    {
+        //Collection is a class that means we can create an object
+        //can be create or declared instance of a class ,so we can add a data in collections
+        //collection types - are designed to store ,manage similar data
+        //We can used to adding ,remove,find and sorting data in collection
+        //two types - Generic and Non-Generic collections
+        //Non-generic - arrayList , Hashtable, sortedlist, stack and Queue
+        //Generic - List ,dictionary,Sortedlist,stack and queue
+        static void Main(string[] args)
+        {
+           /* while (true)
+            {
+                Console.WriteLine("choose the below options");
+                Console.WriteLine("1-List");
+                Console.WriteLine("2-Array");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        ListDemo listDemo = new ListDemo();
+                        listDemo.Adding();
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        ArrayDemo arrayDemo = new ArrayDemo();
+                        arrayDemo.Add();
+                        Console.ReadLine();
+                        break;
+                }
+            }*/
+            
+            
+            ListDemo listDemo = new ListDemo();
+            listDemo.Adding();
+            Console.ReadLine();
+
+        }
+    }
+}
